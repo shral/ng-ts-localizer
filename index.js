f994e0a68299c84f31791b1612084a75a9019be1
shefki <shralsheki@gmail.com>
2020-02-12 16:40:20 +0100

    adding the posibility to itterate through folders

--- index.js
+++ index.js
@@ -9,68 +9,67 @@ var process = require("process");
 
 var workingPath = getPathArgument() || ".";
 
-fs.readdir(workingPath, function (err, files) {
-  if (err) {
-    console.error("Could not list the directory.", err);
-    process.exit(1);
-  }
+readFolder(workingPath);
 
-  files.forEach(function (file, index) {
-    var fromPath = path.join(workingPath, file);
-
-    console.log("file:",file);
-    if(file.indexOf(".ts") > -1){
-        fs.stat(fromPath, function (error, stat) {
-        if (error) {
-          console.error("Error stating file.", error);
-          return;
-        }
-
-        if (stat.isFile())
-          console.log("'%s' is a file.", fromPath);
-        else if (stat.isDirectory())
-          console.log("'%s' is a directory.", fromPath);
-
-
-        fs.readFile(fromPath, 'utf8', function (err,data) {
-          if (err) {
-            return console.log(err);
-          }
-          var maneNameOfFile = "";
-          const regex = /([\w-_\d]*)./;
-          if ((m = regex.exec(file)) !== null) {
-            maneNameOfFile = m[0];
-          }
-
-          var result = data.replace(/(")([\w\s.\\']*)(")/g, function (match, p1, p2, p3, offset, string) {
-            if(p2.indexOf(" ") > -1 && p1 === p3){
-              return "$localize `:@@"+maneNameOfFile+p2.toLowerCase().replace(/\'/g, "").replace(/\s/g, "_")+":"+p2+"`";
-            }else{
-              return match;
-            }
-          });
-          var endResult = result.replace(/(')([\w\s.\\"]*)(')/g, function (match, p1, p2, p3, offset, string) {
-            if(p2.indexOf(" ") > -1 && p1 === p3){
-              return "$localize `:@@"+maneNameOfFile+p2.toLowerCase().replace(/\'/g, "").replace(/\s/g, "_")+":"+p2+"`";
-            }else{
-              return match;
-            }
-          });
-          fs.writeFile(fromPath, endResult, 'utf8', function (err) {
-             if (err) return console.log(err);
-          });
-        })
-
-      });
+function readFolder(currentPath){
+  fs.readdir(currentPath, function (err, files) {
+    if (err) {
+      console.error("Could not list the directory.", err);
+      process.exit(1);
     }
-  });
-});
 
+    files.forEach(function (file, index) {
+      var fromPath = path.join(currentPath, file);
+
+
+          fs.stat(fromPath, function (error, stat) {
+            if(stat.isFile() && file.indexOf(".ts") > -1){
+                    console.log("file:",file);
+              if (error) {
+                console.error("Error stating file.", error);
+                return;
+              }
+              fs.readFile(fromPath, 'utf8', function (err,data) {
+                if (err) {
+                  return console.log(err);
+                }
+                var maneNameOfFile = "";
+                const regex = /([\w-_\d]*)./;
+                if ((m = regex.exec(file)) !== null) {
+                  maneNameOfFile = m[0];
+                }
+
+                var result = data.replace(/(")([\w\s.\\']*)(")/g, function (match, p1, p2, p3, offset, string) {
+                  if(p2.indexOf(" ") > -1 && p1 === p3){
+                    return "$localize `:@@"+maneNameOfFile+p2.toLowerCase().replace(/\'/g, "").replace(/\s/g, "_")+":"+p2+"`";
+                  }else{
+                    return match;
+                  }
+                });
+                var endResult = result.replace(/(')([\w\s.\\"]*)(')/g, function (match, p1, p2, p3, offset, string) {
+                  if(p2.indexOf(" ") > -1 && p1 === p3){
+                    return "$localize `:@@"+maneNameOfFile+p2.toLowerCase().replace(/\'/g, "").replace(/\s/g, "_")+":"+p2+"`";
+                  }else{
+                    return match;
+                  }
+                });
+                fs.writeFile(fromPath, endResult, 'utf8', function (err) {
+                   if (err) return console.log(err);
+                });
+              })
+
+            }else if (stat.isDirectory()){
+                readFolder(fromPath)
+              }   
+        });
+    });
+  });
+
+}
 
 function getPathArgument(){
   var path;
   process.argv.forEach((p)=>{
-    console.log("p",p);
     if(p.indexOf("--path") > -1){
       path = p.replace(/--path=/,"");
     }
